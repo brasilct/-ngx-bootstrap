57bafab685d1cd438d3688169167a0fc0145685e
bruno da silva pontes <bruno_s.pontes@live.com>
2018-08-31 19:48:23 -0300

    agora vai

--- datepicker/engine/format-days-calendar.js
+++ datepicker/engine/format-days-calendar.js
@@ -1,30 +1,72 @@
-import { formatDate } from '../../chronos/format';
-import { getLocale } from '../../chronos/locale/locales';
+import { formatDate } from "../../chronos/format";
+import { getLocale } from "../../chronos/locale/locales";
 export function formatDaysCalendar(daysCalendar, formatOptions, monthIndex) {
-    return {
-        month: daysCalendar.month,
-        monthTitle: formatDate(daysCalendar.month, formatOptions.monthTitle, formatOptions.locale),
-        yearTitle: formatDate(daysCalendar.month, formatOptions.yearTitle, formatOptions.locale),
-        weekNumbers: getWeekNumbers(daysCalendar.daysMatrix, formatOptions.weekNumbers, formatOptions.locale),
-        weekdays: getShiftedWeekdays(formatOptions.locale),
-        weeks: daysCalendar.daysMatrix.map(function (week, weekIndex) { return ({
-            days: week.map(function (date, dayIndex) { return ({
+  var lastDay = 0;
+
+  return {
+    month: daysCalendar.month,
+    monthTitle: formatDate(
+      daysCalendar.month,
+      formatOptions.monthTitle,
+      formatOptions.locale
+    ),
+    yearTitle: formatDate(
+      daysCalendar.month,
+      formatOptions.yearTitle,
+      formatOptions.locale
+    ),
+    weekNumbers: getWeekNumbers(
+      daysCalendar.daysMatrix,
+      formatOptions.weekNumbers,
+      formatOptions.locale
+    ),
+    weekdays: getShiftedWeekdays(formatOptions.locale),
+    weeks: daysCalendar.daysMatrix.map(function(week, weekIndex) {
+      return {
+        days: week.map(function(date, dayIndex) {
+          try {
+            var day = formatDate(
+              date,
+              formatOptions.dayLabel,
+              formatOptions.locale
+            );
+
+            console.log(day);
+
+            if (day !== lastDay) {
+              lastDay = day;
+
+              return {
                 date: date,
-                label: formatDate(date, formatOptions.dayLabel, formatOptions.locale),
+                label: formatDate(
+                  date,
+                  formatOptions.dayLabel,
+                  formatOptions.locale
+                ),
                 monthIndex: monthIndex,
                 weekIndex: weekIndex,
                 dayIndex: dayIndex
-            }); })
-        }); })
-    };
+              };
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        })
+      };
+    })
+  };
 }
 export function getWeekNumbers(daysMatrix, format, locale) {
-    return daysMatrix.map(function (days) { return (days[0] ? formatDate(days[0], format, locale) : ''); });
+  return daysMatrix.map(function(days) {
+    return days[0] ? formatDate(days[0], format, locale) : "";
+  });
 }
 export function getShiftedWeekdays(locale) {
-    var _locale = getLocale(locale);
-    var weekdays = _locale.weekdaysShort();
-    var firstDayOfWeek = _locale.firstDayOfWeek();
-    return weekdays.slice(firstDayOfWeek).concat(weekdays.slice(0, firstDayOfWeek));
+  var _locale = getLocale(locale);
+  var weekdays = _locale.weekdaysShort();
+  var firstDayOfWeek = _locale.firstDayOfWeek();
+  return weekdays
+    .slice(firstDayOfWeek)
+    .concat(weekdays.slice(0, firstDayOfWeek));
 }
 //# sourceMappingURL=format-days-calendar.js.map
