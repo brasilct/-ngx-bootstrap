5dc87f0bc5af080075fb2d13c72e5f56dbea7d95
bruno da silva pontes <bruno_s.pontes@live.com>
2018-08-31 22:57:46 -0300

    e lá vamos nós

--- datepicker/utils/matrix-utils.js
+++ datepicker/utils/matrix-utils.js
@@ -1,14 +1,32 @@
-import { shiftDate } from '../../chronos/utils/date-setters';
+import { shiftDate } from "../../chronos/utils/date-setters";
 export function createMatrix(options, fn) {
-    var prevValue = options.initialDate;
-    var matrix = new Array(options.height);
-    for (var i = 0; i < options.height; i++) {
-        matrix[i] = new Array(options.width);
-        for (var j = 0; j < options.width; j++) {
+  var prevValue = options.initialDate;
+  var matrix = new Array(options.height);
+  for (var i = 0; i < options.height; i++) {
+    matrix[i] = new Array(options.width);
+
+    for (var j = 0; j < options.width; j++) {
+      if (i > 0) {
+        if (j === 0) {
+          if (fn(prevValue) !== matrix[i - 1][6]) {
+            console.log(fn(prevValue) + "-" + matrix[i - 1][6]);
             matrix[i][j] = fn(prevValue);
             prevValue = shiftDate(prevValue, options.shift);
+          }
+        } else {
+          if (fn(prevValue) !== matrix[i][j - 1]) {
+            console.log(fn(prevValue) + "-" + matrix[i][j - 1]);
+            matrix[i][j] = fn(prevValue);
+            prevValue = shiftDate(prevValue, options.shift);
+          }
         }
+      } else {
+        matrix[i][j] = fn(prevValue);
+        prevValue = shiftDate(prevValue, options.shift);
+        console.log(fn(prevValue) + "-" + matrix[i][j]);
+      }
     }
-    return matrix;
+  }
+  return matrix;
 }
 //# sourceMappingURL=matrix-utils.js.map
