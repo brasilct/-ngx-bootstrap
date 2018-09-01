56bf9546241f18a0f58362d8d08d49cc37820c9d
bruno da silva pontes <bruno_s.pontes@live.com>
2018-08-31 21:25:57 -0300

    teasteasdasd

--- chronos/create/date-from-array.js
+++ chronos/create/date-from-array.js
@@ -14,10 +14,19 @@ export function createDate(y, m, d, h, M, s, ms) {
     if (s === void 0) { s = 0; }
     if (ms === void 0) { ms = 0; }
     var date = new Date(y, m, d, h, M, s, ms);
+
+    var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+    date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+
+    var newDate = new Date(now_utc);
+
     // the date constructor remaps years 0-99 to 1900-1999
-    if (y < 100 && y >= 0 && isFinite(date.getFullYear())) {
-        date.setFullYear(y);
+    if (y < 100 && y >= 0 && isFinite(newDate.getFullYear())) {
+        newDate.setFullYear(y);
     }
-    return date;
+
+    console.log(newDate);
+
+    return newDate;
 }
 //# sourceMappingURL=date-from-array.js.map
