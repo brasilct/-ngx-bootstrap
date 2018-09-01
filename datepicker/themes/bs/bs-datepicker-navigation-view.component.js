aa5a5cf9f76c42e9a9de68426e4a403ae7209fe8
carlos.vieira <carlos.vieira@BRCT-SP-N1073>
2018-09-01 00:25:25 -0300

    Alteracao Calendar

--- datepicker/themes/bs/bs-datepicker-navigation-view.component.js
+++ datepicker/themes/bs/bs-datepicker-navigation-view.component.js
@@ -1,21 +1,61 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { BsNavigationDirection } from '../../models/index';
+import { calendar } from '../../../chronos/moment/calendar';
 var BsDatepickerNavigationViewComponent = (function () {
     function BsDatepickerNavigationViewComponent() {
         this.onNavigate = new EventEmitter();
         this.onViewMode = new EventEmitter();
+        this.onSelect = new EventEmitter();
     }
-    BsDatepickerNavigationViewComponent.prototype.navTo = function (down) {
-        this.onNavigate.emit(down ? BsNavigationDirection.DOWN : BsNavigationDirection.UP);
-    };
+    BsDatepickerNavigationViewComponent.prototype.navTo = function (value, down) {
+        debugger;
+        let month = value.month.getMonth();
+        let newMonth = 1;
+        if (down) {         
+            newMonth = BsNavigationDirection.DOWN;          
+        } else {         
+            newMonth = BsNavigationDirection.UP;          
+        }
+        
+        if(month == 9){
+
+            var data = {
+                date: Date,
+                label: String,
+                isDisabled: Boolean,
+                isHovered: Boolean
+              };
+                          
+
+            value.month.setMonth(value.month.getMonth() + 1);
+            
+            data.date = value.month;
+            data.label = 'novembro';
+            data.isHovered = true;
+            data.isDisabled = false;
+            
+            this.onSelect.emit(data);
+
+            this.onNavigate.emit(newMonth);
+            
+
+
+        }
+        else{       
+             this.onNavigate.emit(newMonth);
+        }
+
+      };
     BsDatepickerNavigationViewComponent.prototype.view = function (viewMode) {
+        debugger;
+
         this.onViewMode.emit(viewMode);
     };
     BsDatepickerNavigationViewComponent.decorators = [
         { type: Component, args: [{
                     selector: 'bs-datepicker-navigation-view',
                     changeDetection: ChangeDetectionStrategy.OnPush,
-                    template: "\n    <button class=\"previous\"\n            [disabled]=\"calendar.disableLeftArrow\"\n            [style.visibility]=\"calendar.hideLeftArrow ? 'hidden' : 'visible'\"\n            (click)=\"navTo(true)\"><span>&lsaquo;</span>\n    </button>\n\n    <button class=\"current\"\n            *ngIf=\"calendar.monthTitle\"\n            (click)=\"view('month')\"\n    ><span>{{ calendar.monthTitle }}</span>\n    </button>\n\n    <button class=\"current\" (click)=\"view('year')\"\n    ><span>{{ calendar.yearTitle }}</span></button>\n\n    <button class=\"next\"\n            [disabled]=\"calendar.disableRightArrow\"\n            [style.visibility]=\"calendar.hideRightArrow ? 'hidden' : 'visible'\"\n            (click)=\"navTo(false)\"><span>&rsaquo;</span>\n    </button>\n  "
+                          template: "\n    <button class=\"previous\"\n            [disabled]=\"calendar.disableLeftArrow\"\n            [style.visibility]=\"calendar.hideLeftArrow ? 'hidden' : 'visible'\"\n            (click)=\"navTo(calendar, true)\"><span>&lsaquo;</span>\n    </button>\n\n    <button class=\"current\"\n            *ngIf=\"calendar.monthTitle\"\n            (click)=\"view('month')\"\n    ><span>{{ calendar.monthTitle }}</span>\n    </button>\n\n    <button class=\"current\" (click)=\"view('year')\"\n    ><span>{{ calendar.yearTitle }}</span></button>\n\n    <button class=\"next\"\n            [disabled]=\"calendar.disableRightArrow\"\n            [style.visibility]=\"calendar.hideRightArrow ? 'hidden' : 'visible'\"\n            (click)=\"navTo(calendar, false)\"><span>&rsaquo;</span>\n    </button>\n  "
                 },] },
     ];
     /** @nocollapse */
@@ -24,7 +64,10 @@ var BsDatepickerNavigationViewComponent = (function () {
         'calendar': [{ type: Input },],
         'onNavigate': [{ type: Output },],
         'onViewMode': [{ type: Output },],
+        'onSelect': [{ type: Output },],
     };
+
+    debugger;
     return BsDatepickerNavigationViewComponent;
 }());
 export { BsDatepickerNavigationViewComponent };
